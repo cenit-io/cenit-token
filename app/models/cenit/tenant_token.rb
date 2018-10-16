0491f60c95f438b3fbedf343ba22aca8a4f22a08
Maikel Arcia Carrazana <macarci@gmail.com>
2018-10-16 16:12:20 -0300

    Catching tenants smoothly

--- app/models/cenit/tenant_token.rb
+++ app/models/cenit/tenant_token.rb
@@ -16,9 +16,12 @@ module Cenit
     included do
       belongs_to :tenant, class_name: Cenit::MultiTenancy.tenant_model_name, inverse_of: nil
 
-      before_create do
-        self.tenant = Cenit::MultiTenancy.tenant_model.current_tenant
-      end
+      before_save :catch_tenant
+    end
+
+    def catch_tenant
+      self.tenant ||= Cenit::MultiTenancy.tenant_model.current_tenant if new_record?
+      true
     end
 
     def set_current_tenant!
